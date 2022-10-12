f6d1f6d3f72ee0585e5522dd5749c7636232de8e
raed rdhaounia <raedrdhaounia@gmail.com>
2022-10-12 07:03:30 +0100

    update register component

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -29,13 +29,13 @@ const Register = () => {
     name: false,
     email: false,
     password: false,
-    agreed: false,
+    agreed: true,
   });
   const [registration, setRegistration] = useState<IRegistration>({
     name: '',
     email: '',
     password: '',
-    agreed: false,
+    agreed: true,
   });
   const {assets, colors, gradients, sizes} = useTheme();
 
@@ -52,7 +52,13 @@ const Register = () => {
       console.log('handleSignUp', registration);
     }
   }, [isValid, registration]);
-
+  const handleSignIn = useCallback(() => {
+    if (!Object.values(isValid).includes(false)) {
+      /** send/save registratin data */
+      navigation.navigate("Home")
+    }
+    navigation.navigate("Home")
+  }, []);
   useEffect(() => {
     setIsValid((state) => ({
       ...state,
@@ -62,7 +68,7 @@ const Register = () => {
       agreed: registration.agreed,
     }));
   }, [registration, setIsValid]);
-
+const [change, setChange] = useState(true)
   return (
     <Block safe marginTop={sizes.md}>
       <Block paddingHorizontal={sizes.s}>
@@ -70,28 +76,9 @@ const Register = () => {
           <Image
             background
             resizeMode="cover"
-            padding={sizes.sm}
             radius={sizes.cardRadius}
-            source={assets.background}
+            source={assets.backgroundMosque}
             height={sizes.height * 0.3}>
-            <Button
-              row
-              flex={0}
-              justify="flex-start"
-              onPress={() => navigation.goBack()}>
-              <Image
-                radius={0}
-                width={10}
-                height={18}
-                color={colors.white}
-                source={assets.arrow}
-                transform={[{rotate: '180deg'}]}
-              />
-              <Text p white marginLeft={sizes.s}>
-                {t('common.goBack')}
-              </Text>
-            </Button>
-
             <Text h4 center white marginBottom={sizes.md}>
               {t('register.title')}
             </Text>
@@ -118,7 +105,7 @@ const Register = () => {
               tint={colors.blurTint}
               paddingVertical={sizes.sm}>
               <Text p semibold center>
-                {t('register.subtitle')}
+                {change? t('register.subtitle'):t('register.subtitle1') }
               </Text>
               {/* social buttons */}
               <Block row center justify="space-evenly" marginVertical={sizes.m}>
@@ -183,7 +170,7 @@ const Register = () => {
                   placeholder={t('common.namePlaceholder')}
                   success={Boolean(registration.name && isValid.name)}
                   danger={Boolean(registration.name && !isValid.name)}
-                  onChangeText={(value) => handleChange({name: value})}
+                  onChangeText={(value) =>  handleChange({name: value})}
                 />
                 <Input
                   autoCapitalize="none"
@@ -207,11 +194,11 @@ const Register = () => {
                 />
               </Block>
               {/* checkbox terms */}
-              <Block row flex={0} align="center" paddingHorizontal={sizes.sm}>
+               <Block row flex={0} align="center" paddingHorizontal={sizes.sm}>
                 <Checkbox
                   marginRight={sizes.sm}
                   checked={registration?.agreed}
-                  onPress={(value) => handleChange({agreed: value})}
+                  onPress={() =>handleChange({agree: !registration.agreed})}
                 />
                 <Text paddingRight={sizes.s}>
                   {t('common.agree')}
@@ -225,26 +212,43 @@ const Register = () => {
                 </Text>
               </Block>
               <Button
-                onPress={handleSignUp}
+                onPress={() => {change? handleSignIn():handleSignUp()}}
                 marginVertical={sizes.s}
                 marginHorizontal={sizes.sm}
                 gradient={gradients.primary}
                 disabled={Object.values(isValid).includes(false)}>
                 <Text bold white transform="uppercase">
-                  {t('common.signup')}
+                  {change ? t('common.signup'): t('common.signin')}
                 </Text>
               </Button>
-              <Button
-                primary
-                outlined
-                shadow={!isAndroid}
-                marginVertical={sizes.s}
-                marginHorizontal={sizes.sm}
-                onPress={() => navigation.navigate('Pro')}>
-                <Text bold primary transform="uppercase">
-                  {t('common.signin')}
+              <Block
+                row
+                flex={0}
+                align="center"
+                justify="center"
+                marginBottom={sizes.sm}
+                paddingHorizontal={sizes.xxl}>
+                <Block
+                  flex={0}
+                  height={1}
+                  width="50%"
+                  end={[1, 0]}
+                  start={[0, 1]}
+                  gradient={gradients.divider}
+                />
+                <Text center marginHorizontal={sizes.s}>
+                  {change? t('register.switch'):t('register.switch1')}
                 </Text>
-              </Button>
+                <Text color={colors.link} onPress={() => setChange(!change)}>{change? t('register.signin'): t('register.signup')}</Text>
+                <Block
+                  flex={0}
+                  height={1}
+                  width="50%"
+                  end={[0, 1]}
+                  start={[1, 0]}
+                  gradient={gradients.divider}
+                />
+              </Block>
             </Block>
           </Block>
         </Block>
